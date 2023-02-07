3040f19c9d1a25548d2512bb20e183f06dacc048
kybrian <kybrian.se@gmail.com>
2023-02-07 16:27:09 +0300

    Analyzing Data

---
+++ 05_analyzing_dataframes.py
@@ -0,0 +1,13 @@
+import pandas as pd 
+
+df = pd.read_csv('data.csv')
+
+
+# print(df.head(10))
+
+# print()
+# print("Last 5 rows of the dataframe")
+# print(df.tail())
+
+# print("Info")
+print(df.info())
